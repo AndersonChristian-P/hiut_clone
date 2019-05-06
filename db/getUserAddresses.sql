6cb4ec4dee0cc507d7199d9d81b5b92c97bf8355
Christian Anderson <ca96187@gmail.com>
2019-05-06 08:18:57 -0600

    modified getUserAddresses.sql to return name

--- db/getUserAddresses.sql
+++ db/getUserAddresses.sql
@@ -2,7 +2,7 @@
 -- FROM addresses
 
 -- need to confirm that the tie in from what is coming from the client side is correct
-SELECT street, city, state, zip
+SELECT street, city, state, zip, firstname, lastname
 FROM addresses a
 JOIN users u
 ON a.user_id = u.user_id
