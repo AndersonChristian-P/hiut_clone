0d38a57e87dba37406b8e384149e98ab9d523bc2
Christian Anderson <ca96187@gmail.com>
2019-05-08 08:58:37 -0600

    coded client side ability to see product details

--- src/components/Mens/MensProduct.js
+++ src/components/Mens/MensProduct.js
@@ -1,18 +1,38 @@
 import React, { Component } from "react"
+import axios from "axios"
 
 class MensProduct extends Component {
   constructor() {
     super()
 
     this.state = {
-
+      product: [{}],
     }
   }
 
+  componentDidMount() {
+    axios.get(`/api/product/men/${this.props.match.params.productId}`)
+      .then(res => {
+        this.setState({
+          product: res.data
+        })
+      })
+  }
+
+
   render() {
+    const { product } = this.state
+
     return (
       <div>
         <h1>This is the Men's Product page.</h1>
+        <img src={product[0].img1} alt="#" />
+        {/* <img src={product[0].img2} alt="#" />
+        <img src={product[0].img3} alt="#" />
+        <img src={product[0].img4} alt="#" />
+        <img src={product[0].img5} alt="#" />
+        <img src={product[0].img6} alt="#" />
+        <img src={product[0].img7} alt="#" /> */}
 
         <button>Add to Cart</button>
       </div>
--- src/components/Womens/WomensProduct.js
+++ src/components/Womens/WomensProduct.js
@@ -1,18 +1,31 @@
 import React, { Component } from "react"
+import axios from "axios"
 
 class WomensProduct extends Component {
   constructor() {
     super()
 
     this.state = {
-
+      product: [{}]
     }
   }
 
+  componentDidMount() {
+    axios.get(`/api/product/women/${this.props.match.params.productId}`)
+      .then(res => {
+        this.setState({
+          product: res.data
+        })
+      })
+  }
+
   render() {
+    const { product } = this.state
+
     return (
       <div>
         <h1>This is the Women's Product page.</h1>
+        <img src={product[0].img1} alt="#" />
 
         <button>Add to Cart</button>
       </div>
