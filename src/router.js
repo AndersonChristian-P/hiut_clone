bb3c5a68c2543bdc755b9a9ad5e3f6f507405e77
Christian Anderson <ca96187@gmail.com>
2019-05-11 16:55:52 -0600

    added in the remaining routes

--- src/router.js
+++ src/router.js
@@ -10,6 +10,15 @@ import Product from "./components/Product/Product"
 import Cart from "./components/Cart/Cart"
 import CheckoutForm from "./components/Forms/CheckoutForm";
 
+import CustomerService from "./components/Pages/CustomerService/CustomerService"
+import Faq from "./components/Pages/Faq/Faq"
+import FitGuide from "./components/Pages/FitGuide/FitGuide"
+import HowToWash from "./components/Pages/HowToWash/HowToWash"
+import OurStory from "./components/Pages/OurStory/OurStory"
+import Repairs from "./components/Pages/Repairs/Repairs"
+import StoreLocations from "./components/Pages/StoreLocations/StoreLocations"
+import TermsAndConditions from "./components/Pages/TermsAndConditions/TermsAndConditions"
+
 
 export default (
   <Switch>
@@ -20,11 +29,19 @@ export default (
       </Login>
     )} />
     <Route path="/info" component={Details} />
-    <Route path="/addressbook" component={AddressBook} />
+    <Route path="/address-book" component={AddressBook} />
     <Route path="/collections/:sex" component={Collections} />
     <Route path="/product/:sex" component={Product} />
     <Route path="/cart" component={Cart} />
     <Route path="/checkout" component={CheckoutForm} />
+    <Route path="/customer-service" component={CustomerService} />
+    <Route path="/faq" component={Faq} />
+    <Route path="/fit-guide" component={FitGuide} />
+    <Route path="/how-to-wash" component={HowToWash} />
+    <Route path="/our-story" component={OurStory} />
+    <Route path="/repairs" component={Repairs} />
+    <Route path="/store-locations" component={StoreLocations} />
+    <Route path="terms-and-conditions" component={TermsAndConditions} />
   </Switch>
 )
 
