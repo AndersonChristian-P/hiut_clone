470c52080438b7c4e218b3553af28561b8070f18
Christian Anderson <ca96187@gmail.com>
2019-05-11 21:48:33 -0600

    coded mens and womens collection to show all products

--- src/components/Mens/MensCollection.js
+++ src/components/Mens/MensCollection.js
@@ -29,9 +29,41 @@ class Mens extends Component {
       return product.id_text.includes("selvedge")
     }).map((product, i) => {
       return (
-        <div key={i}>
+        <div key={i} className="selvedge-product">
           <Link to={`/product/men/${product.id_text}`} >
-            <img width="315" src={product.img0} alt="#" />
+            <img width="315px" src={product.img0} alt="#" />
+          </Link>
+          <h2>{product.fit}</h2>
+          <p>{`${product.men_prod_name1} ${product.men_prod_name2}`}</p>
+          <p>{`£${product.price}`}</p>
+          <p>{product.brief_fit_desc}</p>
+        </div>
+      )
+    })
+
+    let organicProducts = products.filter((product) => {
+      return product.id_text.includes("organic")
+    }).map((product, i) => {
+      return (
+        <div key={i} className="organic-product">
+          <Link to={`/product/men/${product.id_text}`} >
+            <img width="315px" src={product.img0} alt="#" />
+          </Link>
+          <h2>{product.fit}</h2>
+          <p>{`${product.men_prod_name1} ${product.men_prod_name2}`}</p>
+          <p>{`£${product.price}`}</p>
+          <p>{product.brief_fit_desc}</p>
+        </div>
+      )
+    })
+
+    let andersonProduct = products.filter((product) => {
+      return product.id_text.includes("wide")
+    }).map((product, i) => {
+      return (
+        <div key={i} className="anderson-product">
+          <Link to={`/product/men/${product.id_text}`} >
+            <img width="1255px" src={product.img0} alt="#" />
           </Link>
           <h2>{product.fit}</h2>
           <p>{`${product.men_prod_name1} ${product.men_prod_name2}`}</p>
@@ -47,7 +79,7 @@ class Mens extends Component {
 
         <div>
           <h1>Selvedge Denim</h1>
-          <p>Selvedge is an investment. Ours is from Kuroki, the atisanal Japanese denim mill. Woven on a 1959 loom. 100% indigo dyed. Unwashed 14.50z.</p>
+          <p>Selvedge is an investment. Ours is from Kuroki, the atisanal Japanese denim mill. Woven on a 1959 loom. 100% indigo dyed. Unwashed 14.5oz.</p>
           <div>
             {selvedgeProducts}
           </div>
@@ -56,11 +88,17 @@ class Mens extends Component {
         <div>
           <h1>Organic Denim</h1>
           <p>This is our organic denim. It's a beautiful unwashed 12oz denim from a great mill in Turkey. Hard wearing too. We are in organic denim forever. Preferably longer.</p>
+          <div>
+            {organicProducts}
+          </div>
         </div>
 
         <div>
           <h1>The Anderson</h1>
           <p>This is our new fit. A wider leg. A higher rise. It is perfect for those with a bigger thigh, or just prefer to wear jeans a little looser.</p>
+          <div>
+            {andersonProduct}
+          </div>
         </div>
 
       </div>
--- src/components/Womens/WomensCollection.js
+++ src/components/Womens/WomensCollection.js
@@ -30,7 +30,67 @@ class Womens extends Component {
       return product.id_text.includes("coda")
     }).map((product, i) => {
       return (
-        <div key={i}>
+        <div key={i} className="coda-product">
+          <Link to={`/product/women/${product.id_text}`} >
+            <img width="1260" src={product.img0} alt="#" />
+          </Link>
+          <h2>{product.fit_desc}</h2>
+          <p>{product.denim_type}</p>
+          <p>{`£${product.price}`}</p>
+        </div>
+      )
+    })
+
+    let girlfriendProduct = products.filter((product) => {
+      return product.id_text.includes("girlfriend")
+    }).map((product, i) => {
+      return (
+        <div key={i} className="girlfriend-product" >
+          <Link to={`/product/women/${product.id_text}`} >
+            <img width="1260" src={product.img0} alt="#" />
+          </Link>
+          <h2>{product.fit_desc}</h2>
+          <p>{product.denim_type}</p>
+          <p>{`£${product.price}`}</p>
+        </div>
+      )
+    })
+
+    let dinaProduct = products.filter((product) => {
+      return product.id_text.includes("dina")
+    }).map((product, i) => {
+      return (
+        <div key={i} className="dina-product" >
+          <Link to={`/product/women/${product.id_text}`} >
+            <img width="1260" src={product.img0} alt="#" />
+          </Link>
+          <h2>{product.fit_desc}</h2>
+          <p>{product.denim_type}</p>
+          <p>{`£${product.price}`}</p>
+        </div>
+      )
+    })
+
+    let stelsbyProduct = products.filter((product) => {
+      return product.id_text.includes("stelsby-low")
+    }).map((product, i) => {
+      return (
+        <div key={i} className="stelsby-product" >
+          <Link to={`/product/women/${product.id_text}`} >
+            <img width="1260" src={product.img0} alt="#" />
+          </Link>
+          <h2>{product.fit_desc}</h2>
+          <p>{product.denim_type}</p>
+          <p>{`£${product.price}`}</p>
+        </div>
+      )
+    })
+
+    let stelsbyHwProduct = products.filter((product) => {
+      return product.id_text.includes("high-waist")
+    }).map((product, i) => {
+      return (
+        <div key={i} className="stelsby-hw-product" >
           <Link to={`/product/women/${product.id_text}`} >
             <img width="1260" src={product.img0} alt="#" />
           </Link>
@@ -57,11 +117,33 @@ class Womens extends Component {
         <div>
           <h1>The Girlfriend Jean</h1>
           <p>Lower in the rise, sitting just above the ankle. 13.oz denim. Rinsed once. It has quickly become the most sought after cut of the year</p>
+          <div>
+            {girlfriendProduct}
+          </div>
         </div>
 
         <div>
           <h1>The Dina</h1>
           <p>High waist, skinny fit. Understated. Classic. Made with a beautiful raven black stretch denim from Candiani, Italy</p>
+          <div>
+            {dinaProduct}
+          </div>
+        </div>
+
+        <div>
+          <h1>The Stelsby</h1>
+          <p>The Stelsby is the result of 18 months spent perfecting our new Skinny cut. Our first stretch denim using Isko's super innovative 'Reform' denim. Looks good. Feels good.</p>
+          <div>
+            {stelsbyProduct}
+          </div>
+        </div>
+
+        <div>
+          <h1>The Stelsby - High Waist</h1>
+          <p>We've tweaked our women's skinny fit jean to add three centimeters to the rise, making these the perfect fit if you have a curvy shape and find our classic Stelsby a little too boyish.</p>
+          <div>
+            {stelsbyHwProduct}
+          </div>
         </div>
 
       </div>
