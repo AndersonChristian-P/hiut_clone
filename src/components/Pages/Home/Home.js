518c3c40dabd8c5cb8dcbf68598ac42d9c20d4a4
Christian Anderson <ca96187@gmail.com>
2019-05-12 09:58:28 -0600

    started styling the Home component

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,9 +1,50 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
     <div>
-      <h1>This is the Home / Landing page.</h1>
+      <Link to="/">
+        <section className="home-hero">
+          <div className="home-hero-wrap topleft">
+            <h2>
+              Do One
+            <br />
+              Thing Well.
+          </h2>
+          </div>
+        </section>
+
+        <section className="home-partition cf" >
+
+          <article className="article cf denim first">
+            <div>
+              <img className="icon-denim" src="https://s3.us-east-2.amazonaws.com/hiut-clone/Icons/icon-denim.png" alt="#" />
+            </div>
+            <div>
+
+            </div>
+            <Link to="/collections/mens" >
+              <figure>
+                <img className="article-image-men" src="https://s3.us-east-2.amazonaws.com/hiut-clone/Pages/home_page/menhome.jpg" alt="#" />
+              </figure>
+            </Link>
+          </article>
+
+
+
+
+          {/* <article className="article cf denim second">
+            <Link to="/collections/women">
+              <figure>
+                <img src="https://s3.us-east-2.amazonaws.com/hiut-clone/Pages/home_page/womenhome.jpg" alt="#" />
+              </figure>
+            </Link>
+          </article> */}
+
+
+        </section>
+      </Link>
     </div>
   )
 }
