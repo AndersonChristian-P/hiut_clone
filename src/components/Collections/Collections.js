d53275a8b82870b08bfe8a5aa0d5a8dffc57dbfc
Christian Anderson <ca96187@gmail.com>
2019-05-12 09:58:01 -0600

    created AllCollection component and added as a sub-route in the Collections component

--- src/components/Collections/Collections.js
+++ src/components/Collections/Collections.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import { Switch, Route } from "react-router-dom"
 import MensCollection from "./../Mens/MensCollection"
 import WomensCollection from "./../Womens/WomensCollection"
+import AllCollection from "./../Pages/AllCollection/AllCollection"
 
 class Collections extends Component {
 
@@ -10,6 +11,7 @@ class Collections extends Component {
       <Switch>
         <Route path="/collections/mens" component={MensCollection} />
         <Route path="/collections/womens" component={WomensCollection} />
+        <Route path="/collections/all" component={AllCollection} />
       </Switch>
     )
   }
---
+++ src/components/Pages/AllCollection/AllCollection.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+const AllCollection = () => {
+  return (
+    <div>
+      <h1>This is the All Collection Page</h1>
+    </div>
+  )
+}
+
+export default AllCollection
