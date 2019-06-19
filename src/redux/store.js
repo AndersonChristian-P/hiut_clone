5c71000267cec09bfc178436d5f9cc1c08430736
Christian Anderson <ca96187@gmail.com>
2019-06-19 14:54:54 -0600

    added code to use Redux DevTools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from "redux"
+import { createStore, combineReducers, applyMiddleware, compose } from "redux"
 import promiseMiddleware from "redux-promise-middleware"
 import authReducer from "./authReducer"
 import cartReducer from "./cartReducer"
@@ -8,6 +8,12 @@ const rootReducer = combineReducers({
   cart: cartReducer
 })
 
-const store = createStore(rootReducer, applyMiddleware(promiseMiddleware))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const enhancer = composeEnhancers(applyMiddleware(promiseMiddleware))
+
+const store = createStore(rootReducer, enhancer)
+
+// const store = createStore(rootReducer, applyMiddleware(promiseMiddleware))
 
 export default store
